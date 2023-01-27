bc507c8b3fef6c9b07c93010c2a3788972a68682
Tatiana <etn88@yandex.ru>
2023-01-27 16:55:56 +0300

    Убрала интерфейс

--- app/src/main/java/ru/otus/daggerhomework/ViewModelReceiver.kt
+++ app/src/main/java/ru/otus/daggerhomework/ViewModelReceiver.kt
@@ -6,13 +6,14 @@ import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import javax.inject.Inject
+import javax.inject.Named
 
 class ViewModelReceiver @Inject constructor(
     private val context: Context,
     private val liveData: MutableLiveData<Int>
-):InterfaceVMReceiver {
+) {
     val colorLiveData: LiveData<Int> = liveData
-    override fun observeColors(): LiveData<Int> {
+    fun observeColors(): LiveData<Int> {
         if (context !is Application) throw RuntimeException("Здесь нужен контекст апликейшена")
         Toast.makeText(context, "Color received", Toast.LENGTH_LONG).show()
         return colorLiveData
