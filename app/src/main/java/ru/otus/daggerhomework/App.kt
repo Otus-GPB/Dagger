e09bdf3c2d74c8d5a74d22a686e343c9d491d8a9
Tatiana <etn88@yandex.ru>
2023-01-27 16:53:21 +0300

    Поменяла сразу на констекст

--- app/src/main/java/ru/otus/daggerhomework/App.kt
+++ app/src/main/java/ru/otus/daggerhomework/App.kt
@@ -3,13 +3,12 @@ package ru.otus.daggerhomework
 import android.app.Application
 import ru.otus.daggerhomework.dagger.ApplicationComponent
 import ru.otus.daggerhomework.dagger.DaggerApplicationComponent
-import ru.otus.daggerhomework.dagger.MainActivityComponent
 
 class App :Application() {
     lateinit var appComponent: ApplicationComponent
 
     override fun onCreate() {
         super.onCreate()
-        appComponent = DaggerApplicationComponent.factory().create(this)
+        appComponent = DaggerApplicationComponent.factory().create(this.applicationContext)
     }
 }
