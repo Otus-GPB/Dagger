d745a937df552cf814e58786ceb92b08d52f3709
Tatiana <etn88@yandex.ru>
2023-01-27 16:55:14 +0300

    Замена инициализации

--- app/src/main/java/ru/otus/daggerhomework/dagger/MainActivityModule.kt
+++ app/src/main/java/ru/otus/daggerhomework/dagger/MainActivityModule.kt
@@ -2,21 +2,28 @@ package ru.otus.daggerhomework.dagger
 
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import ru.otus.daggerhomework.MainActivity
+import ru.otus.daggerhomework.*
 import javax.inject.Named
 
 
-@Module
+@Module(includes = [MainActivityBinder::class])
 class MainActivityModule {
    @ActivitySingleScope
    @Provides
    fun sharedState():MutableLiveData<Int>{
       return MutableLiveData<Int>()
    }
-   @Provides
+}
+@Module
+interface MainActivityBinder{
+   @Binds
    @Named("Activity")
-   fun provideContext(activity: MainActivity): Context = activity
+   fun provideContext(activity: MainActivity): Context
+   @Binds
+   fun toGenerator(generator: ColorGeneratorImpl): ColorGenerator
+
 }
 
